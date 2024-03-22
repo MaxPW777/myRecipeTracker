c6725d59f43b8649514e3ae8bb64aedce7f96704
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-03-22 16:35:58 +0100

    [~] correction bug

--- backend/src/api/users/models/user.models.ts
+++ backend/src/api/users/models/user.models.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserSchema } from './user.schema';
+import { CreateUserDto } from '../dto/create-user.dto'; // Import du DTO CreateUserDto
 
 @Injectable()
 export class UserModel {
--- backend/src/api/users/services/user.service.ts
+++ backend/src/api/users/services/user.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { UserModel } from '../models/user.models';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserDto } from '../dto/user.dto'; // Import du DTO UserDto
+import { User } from '../models/user.schema';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userModel: UserModel) {}
 
-  async register(createUserDto: CreateUserDto): Promise<User> {
-    // Implémentez la logique d'enregistrement de l'utilisateur
-    return this.userModel.create(createUserDto);
+  async register(createUserDto: CreateUserDto): Promise<UserDto> {
+    const createdUser = await this.userModel.create(createUserDto);
+    // Transformez le modèle d'utilisateur en DTO d'utilisateur avant de le retourner
+    return this.mapToUserDto(createdUser);
   }
 
-  async getAllUsers(): Promise<User[]> {
-    // Implémentez la logique pour récupérer tous les utilisateurs
-    return this.userModel.findAll();
+  async getAllUsers(): Promise<UserDto[]> {
+    const users = await this.userModel.findAll();
+    // Transformez les modèles d'utilisateurs en DTO d'utilisateurs avant de les retourner
+    return users.map(user => this.mapToUserDto(user));
   }
 
-  async getUserById(id: string): Promise<User | null> {
-    // Implémentez la logique pour récupérer un utilisateur par son ID
-    return this.userModel.findOneById(id);
+  async getUserById(id: string): Promise<UserDto | null> {
+    const user = await this.userModel.findOneById(id);
+    // Si l'utilisateur n'est pas trouvé, retournez null
+    if (!user) return null;
+    // Transformez le modèle d'utilisateur en DTO d'utilisateur avant de le retourner
+    return this.mapToUserDto(user);
   }
 
-  // Ajoutez d'autres méthodes de service liées aux utilisateurs
+  // Méthode pour mapper un modèle d'utilisateur en DTO d'utilisateur
+  private mapToUserDto(user: User): UserDto {
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email
+    };
+  }
 }

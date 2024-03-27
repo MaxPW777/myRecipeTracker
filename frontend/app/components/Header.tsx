2aa6dd9f493b8fd41a2b1fbb5ce6ff1396323f6f
Maximilian <mpinderwhite@gmail.com>
2024-03-27 22:17:16 +0100

    [+] header for mobile

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 function Header() {
+    const onMobile = false
+    const links = [
+        ['My Recipes', '/myrecipes'],
+        ['My Ingredients', '/ingredients'],
+        ['Settings', '/settings'],
+        ['Categories', '/categories'],
+    ]
     return (
-        <header className='w-full h-6 border-b border-accent rounded-md flex items-center'>
-            <Image src={''} alt='logo' />
-            <nav className='rounded-full border md:bg-black'><ul></ul></nav>
+        <header className='w-full bg-secondarybg h-20 border-b-2 border-accent rounded-b-md flex items-center justify-around text-white'>
+            <h1 className='text-xl'>LOGO</h1>
+            <nav className='rounded-full w-14 h-14 border-2 border-accent md:bg-black'><ul>
+                {onMobile && links.map((route: string[]) => {
+                    return (
+                        <Link key={route[0]} href={route[1]}>{route[0]}</Link>
+                    )
+                })}
+            </ul></nav>
         </header>
     )
 }

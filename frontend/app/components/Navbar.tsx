caa85a1a8d219715d920fd0cc01b5c4017a52869
Maximilian <mpinderwhite@gmail.com>
2024-03-27 22:49:44 +0100

    [+] mobile handling

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,24 +1,56 @@
 "use client"
-import Link from 'next/link'
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react';
+// If you're using Next.js, ensure to import Link from 'next/link'
+import Link from 'next/link';
+
+// Custom hook to check for mobile device
+const useIsMobile = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkIfMobile = () => window.innerWidth < 768;
+        setIsMobile(checkIfMobile());
+
+        const handleResize = () => {
+            setIsMobile(checkIfMobile());
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return isMobile;
+};
 
 function Navbar() {
-    const [onMobile, setOnMobile] = useState<boolean>()
+    const isMobile = useIsMobile();
     const links = [
         ['My Recipes', '/myrecipes'],
         ['My Ingredients', '/ingredients'],
         ['Settings', '/settings'],
         ['Categories', '/categories'],
-    ]
+    ];
+    console.log("fuix");
+    
+    if (isMobile) {
+        return (<nav className='rounded-full w-14 h-14 border-2 border-accent '>
+            {/* Potentially add a hamburger menu or similar for mobile */}
+        </nav>)
+    }
+
     return (
-        <nav className='rounded-full w-14 h-14 border-2 border-accent md:bg-black'><ul>
-            {onMobile && links.map((route: string[]) => {
-                return (
-                    <Link key={route[0]} href={route[1]}>{route[0]}</Link>
-                )
-            })}
-        </ul></nav>
+        <nav className='p-2 h-14 border-2 border-accent '>
+            <ul className='flex items-center justify-between gap-4'>
+                {links.map((route) => {
+                    return (
+                        <li key={route[0]}>
+                            <Link href={route[1]}>{route[0]}</Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Kufam } from "@/ui/fonts"
 
 export const metadata: Metadata = {
   title: "Recipe Tracker",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={Kufam.className}>
         <Header />
         {children}
       </body>

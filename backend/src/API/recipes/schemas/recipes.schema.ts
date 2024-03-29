065b7bd8b840c98a22f673fced5ec165a243cb2a
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-03-29 15:12:24 +0100

    [~] API recipes

--- backend/src/API/recipes/schemas/recipes.schema.ts
+++ backend/src/API/recipes/schemas/recipes.schema.ts
@@ -1,31 +1,33 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { ObjectId } from "mongoose";
+import * as mongoose from 'mongoose';
 
 @Schema({
     timestamps: true,
     collection: 'Recipes'
 })
 export class Recipes {
-    @Prop({ required: true })
+
+    @Prop({ required: true})
     title: string;
 
-    @Prop({ required: true })
+    @Prop({ required:true,type: mongoose.Schema.Types.ObjectId})
     author: ObjectId;
 
-    @Prop({ required: true })
+    @Prop({required: true})
     ingredients: { 
         ingredientId: ObjectId;
         quantity: string;
         optional?: string;
     }[];
 
-    @Prop({ required: true })
+    @Prop({required: true})
     recipeParts: {
         partNumber: number;
         instructions: string;
     }[];
 
-    @Prop([String])
+    @Prop()
     tags: string[];
 }
 
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './api/users/users.module';
+import { RecipesModule } from './API/recipes/recipes.module';
 require('dotenv').config();
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.DB_URL)
-  ,UsersModule], // Importez le module de base de données ici
+  ,UsersModule,RecipesModule], // Importez le module de base de données ici
   controllers: [AppController],
   providers: [AppService],
 })

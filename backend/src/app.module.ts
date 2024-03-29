80d79b14f05ab747c4dad83ffec04325cbfbe1fa
Maximilian <mpinderwhite@gmail.com>
2024-03-29 14:08:54 +0100

    [+] added api directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './api/users/users.module';
 require('dotenv').config();
 
 @Module({

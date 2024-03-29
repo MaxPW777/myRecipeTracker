6ca19b5e99243dc08e63eccd579e33de841b1148
Maximilian <mpinderwhite@gmail.com>
2024-03-29 15:19:18 +0100

    [+] added non functional login page

---
+++ frontend/app/(pages)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/app/(pages)/login/LoginForm";
+
+function LoginPage(){
+    return (
+        <main>
+                <LoginForm/>
+        </main>
+    )
+}
+export default LoginPage;

fc1b7d5c4d5553552b6d279e4966941624afe080
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-04-05 15:14:28 +0200

    [~] ajout ByID recipe

--- backend/src/API/recipes/recipes.controller.ts
+++ backend/src/API/recipes/recipes.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Param, HttpException } from "@nestjs/common";
 import { RecipesService } from "./recipes.service";
 import { Recipes } from "./schemas/recipes.schema";
 import { CreateRecipesDto } from "./dto/create-recipes.dto";
+import { mongo } from "mongoose";
 
 
 @Controller('recipes')
@@ -9,9 +10,17 @@ import { CreateRecipesDto } from "./dto/create-recipes.dto";
 export class RecipesController {
     constructor(private recipesService: RecipesService) { }
 
-    @Get()
-    async getAllRecipes(): Promise<Recipes[]> {
-        return this.recipesService.findAll();
+    @Get(":id")
+    async getRecipeById(@Param('id') id: string): Promise<Recipes> {
+        const isValid = mongo.ObjectId.isValid(id);
+        if (!isValid) {
+            throw new HttpException("Invalid id",400);
+        }
+        const recipe = await this.recipesService.findById(id);
+        if (!recipe) {
+            throw new HttpException("Recipe not found",404);
+        }
+        return recipe;
     }
 
     @Post()
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './api/users/users.module';
 import { RecipesModule } from './API/recipes/recipes.module';
+import { IngredientsModule } from './API/ingredients/ingredients.module';
 require('dotenv').config();
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.DB_URL)
-  ,UsersModule,RecipesModule], // Importez le module de base de données ici
+  ,UsersModule,RecipesModule,IngredientsModule], // Importez le module de base de données ici
   controllers: [AppController],
   providers: [AppService],
 })

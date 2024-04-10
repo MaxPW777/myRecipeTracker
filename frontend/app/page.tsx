8b9ce1d0f940322f4409c3d7fd9b19ec53c7ba47
Maximilian <mpinderwhite@gmail.com>
2024-04-10 16:41:52 +0200

    [~] main page config

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import MyRecipeContainer from './components/MyRecipeContainer'
 
 function HomePage() {
     return (
-        <main>
+        <main className={'justify-center items-center gap-4 flex'}>
             <div className={'main-block p-5 flex flex-col gap-4'}>
                 <p>My Ingredient</p>
                 <div className={'h-full w-full border rounded'}>

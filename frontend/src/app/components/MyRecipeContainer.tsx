b0f85f7fa17bade0c2ca881dd1d3c298c003af27
Maximilian <mpinderwhite@gmail.com>
2024-04-19 14:17:02 +0200

    [~] fixed typing errors

--- frontend/src/app/components/MyRecipeContainer.tsx
+++ frontend/src/app/components/MyRecipeContainer.tsx
@@ -1,31 +1,13 @@
-"use client"
-import Recipe from "@/src/types/Recipe";
 import React from "react";
-import {useGetRecipeByIdQuery} from "@/src/services/recipes";
-
-const recipes: Recipe[] | null = null
 
 function MyRecipeContainer() {
-    const {data} = useGetRecipeByIdQuery('zipette')
-    const [recipes, setRecipes] = React.useState<Recipe[] | null>(null)
 
     return (
         <div className={'main-block'}>
             <p className={'m-5'}>My Recipes</p>
-            {
-                data ?
-                    (<div className={'flex flex-col'}>
-                            {data.map((recipe,index) => (
-                                <div
-                                    className={'w-full h-20 flex flex-col justify-center items-end border-y-2 border-accent text-2xl'}
-                                    key={index}>
-                                    <h1>{recipe.title}</h1>
-                                    <h3 className={'text-sm'}>{recipe.ingredients.length}</h3>
-                                </div>
-                            ))}
-                        </div>
-                    ) : <div className={'flex items-center justify-center mt-[30%]'}>Loading</div>
-            }
+            <div className={'flex flex-col'}>
+
+            </div>
         </div>
     )
 }

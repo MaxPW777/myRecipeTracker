5dc1b20fd487d74e9a85dd4bce191899c3d6cdb3
ludogernay <ludovic.gernay@ynov.com>
2024-04-23 16:35:24 +0200

    [+] register form ended

--- frontend/src/app/(pages)/register/RegisterForm.tsx
+++ frontend/src/app/(pages)/register/RegisterForm.tsx
@@ -1,49 +1,113 @@
 "use client";
-import { useCreateUserMutation } from '@/src/services/users';
+import { useCreateUserMutation, useGetAllUsersQuery } from '@/src/services/users';
 import './register.css';
 import User from '@/src/types/User';
 
-import { FormEvent, useState } from 'react'; // Importez FormEvent et useState depuis 'react'
+import { FormEvent, useState } from 'react';
+
 
 function RegisterForm() {
+    const { data: users, isLoading, isError } = useGetAllUsersQuery();
     const createUserMutation = useCreateUserMutation();
-    const [user, setUser] = useState<User>({ // Utilisez useState pour gérer l'état local de l'utilisateur
+    const [user, setUser] = useState<User>({
         username: '',
         email: '',
         password: ''
     });
 
-    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault(); // Empêche le formulaire de se soumettre normalement
-        try {
-            await createUserMutation.mutateAsync(user); // Utilisez directement l'objet utilisateur
-            alert('Utilisateur créé avec succès!'); // Affichez une alerte pour indiquer que l'utilisateur a été créé avec succès
-            window.location.href = '/'; // Redirigez l'utilisateur vers la page d'accueil
-        } catch (error) {
-            console.error('Une erreur s\'est produite lors de la création de l\'utilisateur:', error);
-        }
-    };
+    const [usernameValid, setUsernameValid] = useState(false);
+    const [mailValid, setMailValid] = useState(false);
+    const [passwordValid, setPasswordValid] = useState(false);
+    const [matching, setMatching] = useState(false);
 
-    // Fonctions pour mettre à jour les champs du formulaire
     const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, username: event.target.value });
+        const username = event.target.value;
+        const isValid = regexUsername.test(username);
+        setUsernameValid(isValid);
+        document.getElementById('usernameError')!.innerText = isValid ? '' : 'Username must be between 6 and 15 characters';
+        setUser({ ...user, username });
     };
 
     const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, email: event.target.value });
+        const email = event.target.value;
+        const isValid = regexMail.test(email);
+        setMailValid(isValid);
+        document.getElementById('emailError')!.innerText = isValid ? '' : 'Invalid email';
+        setUser({ ...user, email });
     };
 
     const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, password: event.target.value });
+        const password = event.target.value;
+        const isValid = regexPassword.test(password);
+        setPasswordValid(isValid);
+        document.getElementById('passwordError')!.innerText = isValid ? '' : 'Password must meet criteria';
+        setUser({ ...user, password });
     };
+
+    const matchPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const confirmPassword = event.target.value;
+        const isValid = confirmPassword === user.password;
+        setMatching(isValid);
+        document.getElementById('confirmError')!.innerText = isValid ? '' : 'Passwords do not match';
+    };
+
+    const checkIfUserExists = async (user: User) => {
+        var res = false;
+        if (users)
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username === user.username) {
+                alert('Username already taken');
+                res = true;
+            }
+            if (users[i].email === user.email) {
+                alert('Email already taken');
+                res = true;
+            }
+        }
+        return res;
+    };
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!mailValid || !passwordValid || !matching || !usernameValid) {
+            return;
+        }
+        if (await checkIfUserExists(user)) {
+            return;
+        }
+        try {
+            createUserMutation.mutate(user);
+            window.location.href = '/';
+        } catch (error) {
+            console.error('Error creating user:', error);
+        }
+    };
+    
+
+    var regexMail = new RegExp ('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+    var regexPassword = new RegExp ('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+[\\]{}|;:,.<>?]).{8,}$');
+    var regexUsername = new RegExp('^.{6,15}$');
 
     return (
         <form className={'flex'} onSubmit={onSubmit}>
             <div className={'flex flex-col gap-4'}>
-                <input name="username" placeholder={'Username'} type="text" onChange={handleUsernameChange} />
-                <input name="email" placeholder={'Email'} type="email" onChange={handleEmailChange} />
-                <input name="password" placeholder={'Password'} type="password" onChange={handlePasswordChange} />
-                <input className={'w-min'} type="submit" />
+                <div>
+                    <input name="username" placeholder={'Username'} type="text" onChange={handleUsernameChange} />
+                    <p className='text-red-500' id='usernameError'></p>
+                </div>
+                <div>
+                    <input name="email" placeholder={'Email'} type="email" onChange={handleEmailChange} />
+                    <p className='text-red-500' id='emailError'></p>
+                </div>
+                <div>
+                    <input name="password" placeholder={'Password'} type="password" onChange={handlePasswordChange} />
+                    <p className='text-red-500' id='passwordError'></p>
+                </div>
+                <div>
+                    <input name="passwordConfirm" placeholder={'Confirm Password'} type="password" onChange={matchPassword}/>
+                    <p className='text-red-500' id='confirmError'></p>
+                </div>
+                <input className={'w-min'} type="submit"/>
             </div>
         </form>
     );
